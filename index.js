7750fdf475a02920c4482f46128db4a9e73c757e
Isaiah <greatpup1940@gmail.com>
2020-07-20 12:46:25 -0400

    Finished hamburger menu

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+let ham = document.querySelector('.ham-links')
+
+let hamburger = document.querySelector('.hamburger')
+
+let clicked = false
+
+function noScroll() {
+    window.scrollTo(0, 0);
+}
+
+
+
+hamburger.addEventListener('click', () => {
+    if (!clicked) {
+        ham.style.display = 'flex'
+        clicked = true
+        window.location.hash = 'top'
+        window.addEventListener('scroll', noScroll)
+
+
+
+    }
+    else if (clicked) {
+        ham.style.display = 'none'
+        clicked = false
+        window.removeEventListener('scroll', noScroll)
+    }
+})
+
 
